48f88cf8fc6fdffe8928818929b92320d9660cd4
Jesse <pokemonjm@gmail.com>
2025-04-05 17:18:45 -0400

    changed fetch address again

--- client/src/components/Pokedex.jsx
+++ client/src/components/Pokedex.jsx
@@ -14,7 +14,7 @@ const Pokedex = () => {
     useEffect(() => {
         (async () => {
             try {
-                const response = await fetch("https://the-j-and-j-archive-production.up.railway.app/api/pokemon");
+                const response = await fetch("https://the-j-and-j-archive-backend-production.up.railway.app/api/pokemon");
                 const data = await response.json();
                 setPokemons(data);
             } catch (error) {
--- client/src/components/util/GetImage.jsx
+++ client/src/components/util/GetImage.jsx
@@ -4,12 +4,12 @@ const GetImage = ({ selectedUser, pokemon, width, height, onClick, isUnsaved, sr
         return (<img className="poke-image" src={src} width={width} height={height} onClick={onClick} />);
     if (selectedUser === "Jesse") {
         if (pokemon.jesselocation !== null && pokemon.jesselocation !== "")
-            return (<img className="poke-image" src={`https://the-j-and-j-archive-production.up.railway.app${pokemon.jesselocation}`} width={width} height={height} onClick={onClick} />);
+            return (<img className="poke-image" src={`https://the-j-and-j-archive-backend-production.up.railway.app${pokemon.jesselocation}`} width={width} height={height} onClick={onClick} />);
         else
             return (<img className="poke-image dont-have" src={pokemon.defaultlocation} width={width} height={height} onClick={onClick} />);
     } else {
         if (pokemon.jasminelocation !== null && pokemon.jasminelocation !== "")
-            return (<img className="poke-image" src={`https://the-j-and-j-archive-production.up.railway.app${pokemon.jasminelocation}`} width={width} height={height} onClick={onClick} />)
+            return (<img className="poke-image" src={`https://the-j-and-j-archive-backend-production.up.railway.app${pokemon.jasminelocation}`} width={width} height={height} onClick={onClick} />)
         else
             return (<img className="poke-image dont-have" src={pokemon.defaultlocation} width={width} height={height} onClick={onClick} />);
     }
